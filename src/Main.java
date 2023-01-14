36f5e87a5e6ec1bca072a35020d983f76f137247
Homework3 <tradervdovin@gmail.com>
2023-01-14 12:07:42 +0500

    ДЗ 8

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        //Пример While
+        System.out.println("пример While");
+        int start = 5;
+        while (start > 0) {
+            System.out.println (start);
+            start = start - 1;
+        }
+        //пример Do-while
+        System.out.println("пример Do- While");
+        int start2 = 5;
+        do {System.out.println (start2);
+            start2 = start2 - 1;
+        }
+        while (start2 > 0);
+
+        //Задача 1
+        System.out.println("Задача 1");
+
+
+    }
+}
